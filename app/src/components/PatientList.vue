eab7958ee7b6c23521897dce29c314c5c375471f
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-11-11 09:30:45 +0500

    feat: add PatientList component for displaying a list of patients

---
+++ app/src/components/PatientList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import PatientListItem from './PatientListItem.vue';
+  const { patients } = defineProps({
+    patients: {
+      type: Array,
+      required: true,
+    },
+  })
+</script>
+<template>
+  <div class="">
+    <div v-for="patient in patients" :key="patient.id">
+      <PatientListItem :patient="patient" />
+    </div>
+  </div>
+</template>
+
