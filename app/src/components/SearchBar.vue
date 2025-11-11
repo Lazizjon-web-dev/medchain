3df3423b187d2cafc7883c076ab531f5b3545cdb
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-11-11 09:33:24 +0500

    feat: add SearchBar component for searching functionality

---
+++ app/src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <SearchIcon />
+
+    <input type="text" class="Search" placeholder="Search..." />
+  </div>
+</template>
+<script setup lang="ts">
+import SearchIcon from "./icons/SearchIcon.vue";
+</script>
+<style scoped>
+@import "tailwindcss";
+div{
+  @apply bg-gray-200 w-full h-fit flex rounded-full justify-between items-center;
+}
+svg {
+  @apply block mx-3;
+}
+input {
+  @apply p-3 w-full rounded-r-full;
+}
+input:focus {
+  @apply outline-none;
+}
+</style>
