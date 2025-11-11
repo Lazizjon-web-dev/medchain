0a91e1f06853724d4ca3b3d65e41a042cfaa49da
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-11-11 09:28:07 +0500

    feat: create initial Vue 3 application structure with main components

---
+++ app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NavigationBar from './components/NavigationBar.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <NavigationBar />
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
