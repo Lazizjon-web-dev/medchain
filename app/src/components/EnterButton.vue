e4213f9ae600b742181ef6fa336382afc0a9065a
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-11-11 09:35:32 +0500

    feat: add EnterButton component with GreaterThanIcon

---
+++ app/src/components/EnterButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import GreaterThanIcon from './icons/GreaterThanIcon.vue';
+</script>
+<template>
+  <button>
+    <GreaterThanIcon />
+  </button>
+</template>
+<style scoped>
+@import "tailwindcss";
+button {
+  @apply w-fit h-fit rounded-full m-1 p-2 bg-gray-300 hover:bg-gray-400; 
+}
+</style>
