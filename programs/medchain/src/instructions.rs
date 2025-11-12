5c8d5f0879b224dbecd24e372fb41b392ede5fc7
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-11-12 11:17:43 +0500

    feat: add AddMedicalRecord struct for handling medical record creation

--- programs/medchain/src/instructions.rs
+++ programs/medchain/src/instructions.rs
@@ -1,4 +1,4 @@
-use crate::state::PatientAccount;
+use crate::state::{MedicalRecord, PatientAccount};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
@@ -9,5 +9,27 @@ pub struct InitializePatient<'info> {
     #[account(init,
         payer = user, space = PatientAccount::LEN, seeds = [b"patient", user.key().as_ref()], bump)]
     pub patient: Account<'info, PatientAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(record_id: u64)]
+pub struct AddMedicalRecord<'info> {
+    #[account(mut)]
+    pub patient: Signer<'info>,
+
+    #[account(mut, has_one = authority, seeds = [b"patient", patient.key().as_ref()], bump = patient_account.bump,)]
+    pub patient_account: Account<'info, PatientAccount>,
+
+    #[account(
+        init,
+        payer = patient,
+        space = MedicalRecord::LEN,
+        seeds = [b"medical_record",
+        patient.key().as_ref(),
+        &record_id.to_le_bytes()],
+        bump
+    )]
+    pub medical_record: Account<'info, MedicalRecord>,
     pub system_program: Program<'info, System>,
 }
