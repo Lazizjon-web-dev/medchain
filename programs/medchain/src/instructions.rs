e2fdd65af2d035564b6be662793ccd41f5acd1b9
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-11-12 11:40:12 +0500

    feat: add GrantAccess struct for managing access grants to medical records

--- programs/medchain/src/instructions.rs
+++ programs/medchain/src/instructions.rs
@@ -1,4 +1,4 @@
-use crate::state::{MedicalRecord, PatientAccount};
+use crate::state::{AccessGrant, MedicalRecord, PatientAccount};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
@@ -31,5 +31,34 @@ pub struct AddMedicalRecord<'info> {
         bump
     )]
     pub medical_record: Account<'info, MedicalRecord>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(doctor_wallet: Pubkey)]
+pub struct GrantAccess<'info> {
+    #[account(mut)]
+    pub patient: Signer<'info>,
+
+    #[account(
+        has_one = authority,
+        seeds = [b"patient", patient.key().as_ref()],
+        bump = patient_account.bump,
+    )]
+    pub patient_account: Account<'info, PatientAccount>,
+
+    // Medical Record to which access is being granted - must belong to the patient
+    pub medical_record: Account<'info, MedicalRecord>,
+
+    // The AccessGrant PDA to be created
+    #[account(
+        init,
+        payer = patient,
+        space = AccessGrant::LEN,
+        seeds = [b"access_grant", medical_record.key().as_ref(), doctor_wallet.as_ref()],
+        bump
+    )]
+    pub access_grant: Account<'info, AccessGrant>,
+
     pub system_program: Program<'info, System>,
 }
