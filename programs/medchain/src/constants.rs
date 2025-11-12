0e220fcb231c66da29bbed61c00a5fccd0b9392f
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-11-12 11:18:51 +0500

    feat: add constants for maximum lengths of various fields

---
+++ programs/medchain/src/constants.rs
@@ -0,0 +1,5 @@
+pub const MAX_NAME_LENGTH: usize = 50;
+pub const MAX_RECORD_TYPE_LENGTH: usize = 20;
+pub const MAX_DESCRIPTION_LENGTH: usize = 200;
+pub const IPFS_HASH_LENGTH: usize = 46;
+pub const ENCRYPTED_KEY_LENGTH: usize = 128;
